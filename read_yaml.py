48184c82176f4992fcd8ae77600d61c83a53d461
Alexander Bauer <sasha@crofter.org>
2014-07-07 23:02:44 -0400

    Remove unused code in read_yaml

--- read_yaml.py
+++ read_yaml.py
@@ -5,14 +5,7 @@ import yaml
 print("Loading yaml/lesson.yaml with extreme precaution.")
 with open('yaml/lesson.yaml', 'r') as f: 
     y = yaml.safe_load(f)
-f.close()
 
 print("y is a ", type(y))
 print("y[1] is a ", type(y[1]))
 print("y[2]['CorrectAnswer'] is ", y[2]['CorrectAnswer'] )
-
-input("Enter to exit")
-
-
-
-
