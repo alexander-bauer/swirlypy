4471b942c05bce9488117908abd39a0731150a22
Alexander Bauer <sasha@crofter.org>
2014-07-10 00:12:30 -0400

    Add more documentation and utility to Question
    Also remove cruft code

--- question.py
+++ question.py
@@ -6,13 +6,18 @@ import abc, yaml
 
 class MissingQuestionFieldException(Exception): pass
 
+# XXX: Add a __str__ method to represent the question in some way.
+# XXX: Add some 'hint' capacity
+# XXX: Provide an inbuilt capability to print errors or respond to
+# incorrect user input.
 class Question(object):
+    """Question is an abstract class used to provide utilities for and
+    embody a generic question to be asked in a course, which provides a
+    prompt, or just text, request user response, and test it for
+    correctness."""
+
     # Mark this class as an abstract.
     __metaclass__ = abc.ABCMeta
-
-    # Note that this class has no requirements. Child classes may
-    # redefine this, however.
-    _requires_ = []
 
     def __init__(self, category, output, **kwargs):
         self.category = category
@@ -47,7 +52,27 @@ class Question(object):
 
     @abc.abstractmethod
     def test_response(self, response):
-        """Test the user's response as returned by get_response."""
+        """Test the user's response as returned by get_response,
+        returning True if successful, and False if not."""
+
+    def execute(self):
+        """Execute the question in the default way, by first printing
+        itself, then asking for a response and testing it in a loop
+        until it is correct."""
+        # XXX: Collect statistics here so that it can be used for
+        # grading.
+
+        # Print the output.
+        self.print()
+
+        # Loop until correct.
+        while True:
+            # Get the user's response.
+            resp = self.get_response()
+
+            # Test it. If correct (True), then break from this loop.
+            if test_response(resp):
+                break
 
     @classmethod
     def load_yaml(cls, file):
