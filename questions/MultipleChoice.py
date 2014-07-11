47be65f2e7f4d8b721d27388c43315494567d890
Alexander Bauer <sasha@crofter.org>
2014-07-10 23:27:37 -0400

    Refactor MultipleChoice question
    It now handles errors more cleanly, and shuffles options between valid
    answers.

--- questions/MultipleChoice.py
+++ questions/MultipleChoice.py
@@ -1,26 +1,32 @@
 from swirlypy.question import CategoryQuestion
+import random
 
 class MultipleChoiceQuestion(CategoryQuestion):
     def get_response(self):
+        # Parse the options and shuffle them, for variety.
         options = self.choices.split(";")
-        # todo: randomize order of choices
-        selection = ""
-        while(selection == ""):
-            n = 0
-            for x in options:
-                n += 1
-                print(n, ": ", x)
-            k = int(input("Select one of the numbered choices: "))-1
-            # todo: try/catch integer; allow user to type string matching
-            # one of the answers.
-            if(k < 0 or k > n-1):
-                selection=""
-                print("Please type an integer between 1 and ", n)
-            else:
-                return(options[k])
+        random.shuffle(options)
+
+        # Loop until the user selects the correct answer.
+        while True:
+            # Begin by printing the menu with numerical identifiers.
+            for i, option in enumerate(options):
+                print(i+1, ": ", option)
+
+            # Get the user's choice and try to return the relevant
+            # answer, catching failures and restarting as appropriate.
+            # Note that invalid input restarts the loop here, but an
+            # incorrect answer will restart the loop outside of the
+            # scope of the function. This allows for grading.
+            # XXX: Try to match strings to choices, too.
+            try:
+                choice = int(input("Select one of the numbered choices: "))-1
+                return options[choice]
+            except (ValueError, IndexError):
+                print("Please pick an integer between 1 and ",
+                        len(options))
+                continue
 
     def test_response(self, response):
-        '''This hard-coded test is a stub, awaiting plug-in tests'''
-        return(response == self.correctanswer)
-
-
+        """Check the response in the simplest way possible."""
+        return response == self.correctanswer
