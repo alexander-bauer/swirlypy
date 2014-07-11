d6a85fbba5488b77e01f7e233228719a1cfde3ec
wilcrofter <bill@crofter.net>
2014-07-11 08:13:25 -0400

    Added pause after text question. Granted, a fancier self.print could obviate the need for it.

--- questions/Text.py
+++ questions/Text.py
@@ -3,3 +3,7 @@ from swirlypy.question import CategoryQuestion
 class TextQuestion(CategoryQuestion):
     def execute(self):
         self.print()
+        self.get_response()
+
+    def get_response(self):
+        x=input('...')
