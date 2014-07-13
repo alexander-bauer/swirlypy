396d974d10b7f958381ba5f893528e359e4b9314
Alexander Bauer <sasha@crofter.org>
2014-07-12 20:54:25 -0400

    Add runcourse flag to only print course info

--- utils/runcourse.py
+++ utils/runcourse.py
@@ -7,12 +7,20 @@ def parse(args):
     parser = argparse.ArgumentParser()
     parser.add_argument("course_path", help="directory of or path to \
             course.yaml file")
+    parser.add_argument("--info", "-i", action="store_true",
+            help="show course information")
     return parser.parse_args(args)
 
 def main(args):
     course = swirlypy.course.Course.load(args.course_path)
-    course.print()
-    course.execute()
+
+    # If --info is supplied, only print metadata.
+    # XXX: We should add more fields so that we can be verbose here.
+    if args.info:
+        course.print()
+    else:
+        course.print()
+        course.execute()
 
 if __name__ == "__main__":
     main(parse(sys.argv[1:]))
