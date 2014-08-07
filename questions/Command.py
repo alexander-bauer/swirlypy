5e45db1e3dfabf6acdd41e250d22ab8a777814e5
Alexander Bauer <sasha@crofter.org>
2014-08-06 23:03:46 -0400

    Add documentation to CommandQuestion

--- questions/Command.py
+++ questions/Command.py
@@ -3,6 +3,11 @@ import swirlypy.colors as colors
 import code, ast, sys
 
 class CommandQuestion(RecordingQuestion):
+    """Gives the user a shell prompt, and compares their inputs to a
+given answer. This is accomplished by parsing both inputs as Abstract
+Syntax Trees, and comparing those directly. Thus, syntactical
+differences, such as single-quotes versus double-quotes, do not
+matter."""
     
     _required_ = [ "answer" ]
     
@@ -16,4 +21,3 @@ class CommandQuestion(RecordingQuestion):
         # it will resort to reference equality.
         # XXX: Find a more elegant way to do this.
         return ast.dump(answer_tree) == ast.dump(response["ast"])
-
