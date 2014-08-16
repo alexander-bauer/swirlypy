8b866bf1307a9337d92a8617b6e3176270b65aae
Alexander Bauer <sasha@crofter.org>
2014-08-16 15:06:05 -0400

    Add setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import subprocess, os
+from distutils.core import setup
+
+print("Obtaining version... ", end="")
+version = subprocess.check_output(["git", "describe", "--dirty=+"],
+        universal_newlines=True)
+print(version)
+
+setup(name='swirlypy',
+        version=version,
+        description='Python courseware',
+        author='Alexander Bauer',
+        author_email='sasha@crofter.org',
+        url='https://github.com/SashaCrofter/swirlypy',
+        scripts=['swirlypy/utils/swirlytool.py'],
+        packages=['swirlypy', 'swirlypy.questions']
+        )
